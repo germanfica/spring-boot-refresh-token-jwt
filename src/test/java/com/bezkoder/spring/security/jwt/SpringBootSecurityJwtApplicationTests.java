bc1a20189b2109a0cf4656b93ba50d345e9996f7
German Fica <germafica@gmail.com>
2022-01-13 19:33:47 -0300

    Migrate from a JUnit4-Based Runner to JUnit5

--- src/test/java/com/bezkoder/spring/security/jwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/bezkoder/spring/security/jwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,11 +1,12 @@
 package com.bezkoder.spring.security.jwt;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+// https://www.baeldung.com/junit-5-runwith
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
